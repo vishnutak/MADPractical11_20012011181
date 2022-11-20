1013152450a4e15050cd06b33195dc90ebd65514
vishnu <vishnutak08@gmail.com>
2022-11-20 16:03:41 +0530

    first commit

---
+++ app/src/main/java/com/example/madpractical11_20012011181/NoteViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madpractical11_20012011181
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_view)
+    }
+}
